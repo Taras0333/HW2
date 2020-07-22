7a4b6674a6fe062964aaac0e4c268a1b9cd4a9b6
Taras <khomynt97@gmail.com>
2020-07-22 18:18:23 +0300

    last

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ let secondNumber = Math.floor(prompt('Write your second number'));
 console.log(secondNumber);
 let check = confirm('Do you want to skip even numbers');
 console.log(check);
+let sum = 0;
 if (check){
-  let sum = 0;
   for (let numbers = firstNumber; numbers <= secondNumber; numbers++){
     if (numbers % 2 !== 0){
+      
       sum += numbers;
     }
   }
-  alert(sum);
 } else{
-  let sum = 0;
     for (let numbersEven = firstNumber; numbersEven <= secondNumber; numbersEven++){
     sum +=numbersEven;
     
     }
-  alert(sum);
 }
-/*let container=document.querySelector("#container");
+  console.log(sum);
+
+let container=document.querySelector("#container");
 container.innerHTML=`
 <p>First number: ${firstNumber};</p>
 <p>Second number: ${secondNumber};</p>
 <p>Total: ${sum};</p>
-`;*/
+`;
